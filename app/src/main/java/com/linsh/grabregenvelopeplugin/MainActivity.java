b98f48c5d5479f76765c04fbec952b590f474495
Senh  Linsh <lyuanxiang0812@126.com>
2018-02-05 01:56:14 +0800

    申请悬浮窗权限

--- app/src/main/java/com/linsh/grabregenvelopeplugin/MainActivity.java
+++ app/src/main/java/com/linsh/grabregenvelopeplugin/MainActivity.java
@@ -7,8 +7,10 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.linsh.lshutils.utils.Permission;
 import com.linsh.utilseverywhere.ADBUtils;
 import com.linsh.utilseverywhere.PermissionUtils;
+import com.linsh.utilseverywhere.ToastUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,16 +22,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onclick(View view) {
-        if (ADBUtils.checkRoot()) {
-            ADBUtils.startAccessibilityService(getPackageName(), GREAccessibilityService7.class.getName());
-        } else {
-            boolean check = PermissionUtils.checkPermission(Manifest.permission.WAKE_LOCK);
-            if (!check) {
-                PermissionUtils.requestPermission(this, Manifest.permission.DISABLE_KEYGUARD, null);
+        if (Permission.SystemAlertWindow.checkOrRequest(this, 101)) {
+            if (ADBUtils.checkRoot()) {
+                ADBUtils.startAccessibilityService(getPackageName(), GREAccessibilityService7.class.getName());
             } else {
-                Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-                startActivity(intent);
+                boolean check = PermissionUtils.checkPermission(Manifest.permission.WAKE_LOCK);
+                if (!check) {
+                    PermissionUtils.requestPermission(this, Manifest.permission.DISABLE_KEYGUARD, null);
+                } else {
+                    Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                    startActivity(intent);
+                }
             }
+        } else {
+            ToastUtils.show("需要悬浮窗权限才能看见我的真身哦~");
         }
     }
 }
